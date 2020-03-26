3bc0955989b286babd53777b1c4636201c548d03
znadder <arthur.ramos@edu.unifil.br>
2020-03-26 15:54:48 -0300

    dev do header de pesquisa

--- src/pages/find/index.js
+++ src/pages/find/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image, Dimensions, TextInput, FlatList } from 'react-native';
+import Select2 from './../../components/ASelect';
 
 const { width } = Dimensions.get("window");
+
+const mockData = [
+    { id: 1, name: 'Insulin Apidra' },
+    { id: 2, name: 'Acitamol' },
+    { id: 3, name: 'Parecetamol' },
+    { id: 4, name: 'Ibiprofeno' },
+];
 
 export default class index extends Component {
 
@@ -9,7 +17,17 @@ export default class index extends Component {
         super(props);
 
         this.state = {
-
+            options: [
+                {
+                    title: 'Open'
+                },
+                {
+                    title: 'Near'
+                },
+                {
+                    title: 'Price'
+                },
+            ]
         }
     }
 
@@ -17,12 +35,66 @@ export default class index extends Component {
 
     }
 
+    renderItem = ({item}) => {
+        return(
+            <TouchableOpacity style={{ marginHorizontal: 4, height: 35, width: 100, borderRadius: 20, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{ color: '#1f1f1f', fontSize: 14 }}>{item.title}</Text>
+            </TouchableOpacity>
+        )
+    }
+
     render() {
         return (
             <View style={{ flex: 1 }}>
+
                 <View style={styles.header}>
-                    
-                    
+
+                    <View style={styles.boxInput1}>
+
+                        <Image style={{ height: 25, width: 25, alignSelf: 'center', marginLeft: 8 }}
+                            source={require("./../../assets/userName.png")} />
+
+                        <TextInput style={{ marginLeft: 8 }}
+                            underlineColorAndroid="transparent"
+                            placeholder="Your name here."
+                            placeholderTextColor="#b7bbc7"
+                        />
+
+                    </View>
+
+                    <View style={styles.boxInput}>
+                        <Image style={{ height: 25, width: 25, alignSelf: 'center', marginLeft: 10 }}
+                            source={require("./../../assets/house.png")} />
+                        <Select2
+                            style={{ alignSelf: 'center' }}
+                            colorTheme={'#ff4c51'}
+                            popupTitle='Select state'
+                            title="Select item"
+                            showSearchBox={false}
+                            cancelButtonText={'Return'}
+                            selectButtonText={'Select'}
+                            data={mockData}
+                            onSelect={data => {
+                                this.setState({ data })
+                            }}
+                            onRemoveItem={data => {
+                                this.setState({ data });
+                            }}
+                        />
+
+                    </View>
+
+                    <FlatList
+                        data={this.state.options}
+                        style={{ marginTop: 10 }}
+                        pagingEnabled={true}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        onMomentumScrollEnd={this._onMomentumScrollEnd}
+                        scrollEventThrottle={16}
+                        renderItem={this.renderItem}
+                    />
+
                 </View>
                 <View style={styles.container}>
 
@@ -35,10 +107,11 @@ export default class index extends Component {
 const styles = StyleSheet.create({
     header: {
         flex: 1,
-        backgroundColor: "black",
+        backgroundColor: "#f0f0f0",
         justifyContent: "center",
         alignItems: 'center',
         shadowColor: "#000",
+        paddingHorizontal: 15,
         shadowOffset: {
             width: 0,
             height: 1,
@@ -49,10 +122,42 @@ const styles = StyleSheet.create({
     },
 
     container: {
-        flex: 3,
+        flex: 3.1,
         backgroundColor: "#fcfeff",
         justifyContent: "center",
         alignItems: 'center',
         paddingHorizontal: 50,
+    },
+
+    boxInput1: {
+        height: 40,
+        width: "100%",
+        borderRadius: 20,
+        backgroundColor: "white",
+        flexDirection: 'row',
+        shadowColor: '#000',
+        shadowOffset: { width: 12, height: 12 },
+        shadowOpacity: 0.20,
+        shadowRadius: 12.00,
+        elevation: 12,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 20,
+    },
+
+    boxInput: {
+        height: 40,
+        width: "100%",
+        borderRadius: 20,
+        backgroundColor: "white",
+        flexDirection: 'row',
+        shadowColor: '#000',
+        shadowOffset: { width: 12, height: 12 },
+        shadowOpacity: 0.20,
+        shadowRadius: 12.00,
+        elevation: 12,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
     },
 })
